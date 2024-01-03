dc87c69e585b5d9b2f408615cb7bf4a69c71b3ce
benjamin5252 <david.l@amaryllo.us>
2024-01-03 15:43:57 +0800

    <feat><show profile posts>

--- src/controllers/posts.ts
+++ src/controllers/posts.ts
@@ -6,18 +6,19 @@ import jwt from "jsonwebtoken"
 import moment from "moment"
 
 export const getPosts = (req: Request, res: Response)=>{
-
+  const userId = req.query.userId;
   const token = req.cookies.accessToken
   if(!token) return res.status(401).json({result: false, ...error(10004)})
 
   jwt.verify(token, "secretkey", (err, userInfo)=>{
     if(err) return res.status(403).json({result: false, ...error(10004)})
 
-    const q = `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
+    const q = userId ? `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) WHERE p.userId = ? ORDER BY p.createdAt DESC` 
+    : `SELECT p.*, u.id AS userId, name, profilePic FROM posts AS p JOIN users AS u ON (u.id = p.userId) 
     LEFT JOIN relationships AS r ON (p.userId = r.followedUserId) WHERE r.followerUserId=? OR p.userId=? ORDER BY p.createdAt DESC`
     
 
-    db.query(q, [userInfo.id, userInfo.id], (err: MysqlError, data: any[])=>{
+    db.query(q, userId? [userId] :  [userInfo.id, userInfo.id], (err: MysqlError, data: any[])=>{
       if (err) return res.status(500).json(err)
       return res.status(200).json({result: true, content: data})
     })

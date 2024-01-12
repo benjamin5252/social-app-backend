6f633e5001b211cb76d8943edc7ce520241679f9
benjamin5252 <david.l@amaryllo.us>
2024-01-12 17:01:54 +0800

    <feat>(<acs2 hpc>)<web socket>

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,8 @@ import cors from "cors"
 import cookieParser from "cookie-parser"
 import multer from "multer"
 import * as fs from 'fs';
-import './websocket/index.js'
+import http from "http"
+import { WebSocketServer } from 'ws'
 
 interface MulterRequest extends Request {
   file: any;
@@ -68,8 +69,26 @@ app.use("/api/auth", authRoutes)
 app.use("/api/relationships", relationshipRoutes)
 
 
+const server = http.createServer(app)
+const wss = new WebSocketServer({ server:server });
+wss.on('connection', function connection(ws) {
+  console.log('A new client Connected!');
+  ws.send('Welcome New Client!');
 
+  ws.on('message', function incoming(message) {
+    console.log('received: %s', message);
 
-app.listen(process.env.PORT, ()=>{
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(message);
+      }
+    });
+    
+  });
+});
+
+server.listen(process.env.PORT, ()=>{
   console.log("Server is listening on port: ", process.env.PORT)
 })
+
+

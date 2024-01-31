1443cba644f629e9d7b7f649f847b2b4786450c1
benjamin5252 <david.l@amaryllo.us>
2024-01-31 09:49:30 +0800

    <feat><cross site cookie>

--- src/controllers/auth.ts
+++ src/controllers/auth.ts
@@ -1,59 +1,76 @@
-import { db } from "../config/mySQLConnect.js"
-import bcrypt from "bcryptjs"
-import error from "../config/errors.js"
-import { Express, Request, Response } from "express"
-import jwt from "jsonwebtoken"
-import { MysqlError  } from 'mysql'
-export const register = (req: Request, res: Response)=>{
-  
+import { db } from "../config/mySQLConnect.js";
+import bcrypt from "bcryptjs";
+import error from "../config/errors.js";
+import { Express, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { MysqlError } from "mysql";
+export const register = (req: Request, res: Response) => {
   //CHECK USER IF EXITS
 
-  const q = "SELECT * FROM users WHERE username = ?"
-  
-  db.query(q, [req.body.username], (err: MysqlError,data: any[])=>{
-    if(err) return res.status(500).json({result: false, ...error(0)})
-    if(data.length) return res.status(409).json({result: false, ...error(10001)})
-    //CREATE A NEW USER
-    //HASH THE PASSWORD
-    const salt = bcrypt.genSaltSync(10)
-    const hashPassword = bcrypt.hashSync(req.body.password, salt)
-
-    const q = "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE (?)"
-    const values = [req.body.username, req.body.email, hashPassword, req.body.name]
-    db.query(q, [values], (err, data)=>{
-      if(err) return res.status(500).json(err)
-      return res.status(200).json({result: true})
-    })
-  })
-
-  
-
-}
-
-export const login = (req: Request, res: Response)=>{
-  const q = "SELECT * FROM users WHERE username = ?"
+  const q = "SELECT * FROM users WHERE username = ?";
 
   db.query(q, [req.body.username], (err: MysqlError, data: any[]) => {
-    if (err) return res.status(500).json(err)
-    
-    if (data.length === 0) return res.status(404).json({result: false, ...error(10002)})
-    const checkPassword = bcrypt.compareSync(req.body.password, data[0].password)
-    if(!checkPassword) return res.status(400).json({result: false, ...error(10003)})
-    const token = jwt.sign({id:data[0].id}, "secretkey")
+    if (err) return res.status(500).json({ result: false, ...error(0) });
+    if (data.length)
+      return res.status(409).json({ result: false, ...error(10001) });
+    //CREATE A NEW USER
+    //HASH THE PASSWORD
+    const salt = bcrypt.genSaltSync(10);
+    const hashPassword = bcrypt.hashSync(req.body.password, salt);
 
-    const {password, ...others} = data[0]
-    let farFuture = new Date(new Date().getTime() + (1000*60*60*24*365*10)); // ~10y
-    res.cookie("accessToken", token, {
-      httpOnly: true,
+    const q =
+      "INSERT INTO users (`username`, `email`, `password`, `name`) VALUE (?)";
+    const values = [
+      req.body.username,
+      req.body.email,
+      hashPassword,
+      req.body.name,
+    ];
+    db.query(q, [values], (err, data) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json({ result: true });
+    });
+  });
+};
+
+export const login = (req: Request, res: Response) => {
+  const q = "SELECT * FROM users WHERE username = ?";
+
+  db.query(q, [req.body.username], (err: MysqlError, data: any[]) => {
+    if (err) return res.status(500).json(err);
+
+    if (data.length === 0)
+      return res.status(404).json({ result: false, ...error(10002) });
+    const checkPassword = bcrypt.compareSync(
+      req.body.password,
+      data[0].password
+    );
+    if (!checkPassword)
+      return res.status(400).json({ result: false, ...error(10003) });
+    const token = jwt.sign({ id: data[0].id }, "secretkey");
+
+    const { password, ...others } = data[0];
+    let farFuture = new Date(
+      new Date().getTime() + 1000 * 60 * 60 * 24 * 365 * 10
+    ); // ~10y
+    res
+      .cookie("accessToken", token, {
+        httpOnly: true,
+        secure: true,
+        expires: farFuture,
+        sameSite: "none",
+      })
+      .status(200)
+      .json({ result: true, content: others });
+  });
+};
+
+export const logout = (req: Request, res: Response) => {
+  res
+    .clearCookie("accessToken", {
       secure: true,
-      expires: farFuture
-    }).status(200).json({result: true, content: others})
-  })
-}
-
-export const logout = (req: Request, res: Response)=>{
-  res.clearCookie("accessToken", {
-    secure: true,
-    sameSite: "none"
-  }).status(200).json({result: true})
-}
+      sameSite: "none",
+    })
+    .status(200)
+    .json({ result: true });
+};
--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,128 @@
-import * as dotenv from 'dotenv';
+import * as dotenv from "dotenv";
 dotenv.config();
-import express from "express"
-const app = express()
-import { URL } from 'url';
-const __filename = new URL('', import.meta.url).pathname;
-const __dirname = new URL('.', import.meta.url).pathname;
-import authRoutes from "./routes/auth.js"
-import commentRoutes from "./routes/comments.js"
-import likeRoutes from "./routes/likes.js"
-import postRoutes from "./routes/posts.js"
-import userRoutes from "./routes/users.js"
-import relationshipRoutes from "./routes/relationships.js"
-import cors from "cors"
-import cookieParser from "cookie-parser"
-import multer from "multer"
-import * as fs from 'fs';
-import http from "http"
-import WebSocket, { WebSocketServer } from 'ws'
-import { MyWebSocket } from './libs/interface.js';
-import { wsLogin, wsUpdateFriendList, wsOnClose, wsSendChatMessage, swSetOnlineUserBroadcast } from './libs/websocket.js';
+import express from "express";
+const app = express();
+import { URL } from "url";
+const __filename = new URL("", import.meta.url).pathname;
+const __dirname = new URL(".", import.meta.url).pathname;
+import authRoutes from "./routes/auth.js";
+import commentRoutes from "./routes/comments.js";
+import likeRoutes from "./routes/likes.js";
+import postRoutes from "./routes/posts.js";
+import userRoutes from "./routes/users.js";
+import relationshipRoutes from "./routes/relationships.js";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import multer from "multer";
+import * as fs from "fs";
+import http from "http";
+import WebSocket, { WebSocketServer } from "ws";
+import { MyWebSocket } from "./libs/interface.js";
+import {
+  wsLogin,
+  wsUpdateFriendList,
+  wsOnClose,
+  wsSendChatMessage,
+  swSetOnlineUserBroadcast,
+} from "./libs/websocket.js";
 
 interface MulterRequest extends Request {
   file: any;
 }
 
-
-
 //MIDDLEWARES
-app.use((req, res, next)=>{
-  res.header("Access-Control-Allow-Credentials", 'true')
-  next()
-})
-app.use(express.json())
-app.use(cors({
-  origin: "http://localhost:3000"
-}))
-app.use(cookieParser())
-app.use(express.static( './public'));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Credentials", "true");
+  next();
+});
+app.use(express.json());
 
-
-
+const allowedOrigins = [
+  "http://localhost:3000",
+  "https://benjamin5252.github.io",
+  "https://social.yinte.cloud",
+  "http://social.yinte.cloud",
+];
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (allowedOrigins.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else if (origin) {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+};
+app.use(cors(corsOptions));
+app.use(cookieParser());
+app.use(express.static("./public"));
 
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
-    const dir = './public/upload'
-    const dir1 = './public'
-    if (!fs.existsSync(dir1)){
-        fs.mkdirSync(dir1);
-        fs.mkdirSync(dir);
+    const dir = "./public/upload";
+    const dir1 = "./public";
+    if (!fs.existsSync(dir1)) {
+      fs.mkdirSync(dir1);
+      fs.mkdirSync(dir);
     }
-    cb(null, dir)
-    
-    
+    cb(null, dir);
   },
   filename: function (req, file, cb) {
-    cb(null, Date.now() + '-' + file.originalname)
-  }
-})
-
-const upload = multer({ storage: storage })
-const uploadCallback = (req: MulterRequest, res)=>{
-  const file = req.file;
-  res.status(200).json(file.filename)
-}
-app.post("/api/upload", upload.single("file"), uploadCallback)
-
-app.use("/api/users", userRoutes)
-app.use("/api/posts", postRoutes)
-app.use("/api/comments", commentRoutes)
-app.use("/api/likes", likeRoutes)
-app.use("/api/auth", authRoutes)
-app.use("/api/relationships", relationshipRoutes)
-
-
-const server = http.createServer(app)
-const wss = new WebSocketServer({ server:server });
-
-
-swSetOnlineUserBroadcast(wss)
-
-wss.on('connection', function connection(ws: MyWebSocket) {
-  console.log('A new client Connected!');
-  ws.send('Welcome New Client!');
-  
-
-    ws.on('message', (message: string) => {
-        console.log(`Received: ${message}`);
-
-   
-        try {
-            const msgObj = JSON.parse(message);
-            console.log('Parsed JSON:', msgObj);
-
-            switch(msgObj.method){
-              case('login'):
-                wsLogin(wss, ws, msgObj)
-                break;
-              case('updateFriendList'):
-                wsUpdateFriendList(wss, ws, msgObj)
-                break;
-              case('sendChatMessage'):
-                wsSendChatMessage(wss, ws, msgObj)
-                break;
-            }
-              
-        } catch (error) {
-            console.error('Error parsing JSON');
-        }
-    });
-
-    ws.on('close', () => {
-        console.log('Client disconnected');
-        wsOnClose(wss, ws)
-    });
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
 
-server.listen(process.env.PORT, ()=>{
-  console.log("Server is listening on port: ", process.env.PORT)
-})
+const upload = multer({ storage: storage });
+const uploadCallback = (req: MulterRequest, res) => {
+  const file = req.file;
+  res.status(200).json(file.filename);
+};
+app.post("/api/upload", upload.single("file"), uploadCallback);
 
+app.use("/api/users", userRoutes);
+app.use("/api/posts", postRoutes);
+app.use("/api/comments", commentRoutes);
+app.use("/api/likes", likeRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/relationships", relationshipRoutes);
 
+const server = http.createServer(app);
+const wss = new WebSocketServer({ server: server });
+
+swSetOnlineUserBroadcast(wss);
+
+wss.on("connection", function connection(ws: MyWebSocket) {
+  console.log("A new client Connected!");
+  ws.send("Welcome New Client!");
+
+  ws.on("message", (message: string) => {
+    console.log(`Received: ${message}`);
+
+    try {
+      const msgObj = JSON.parse(message);
+      console.log("Parsed JSON:", msgObj);
+
+      switch (msgObj.method) {
+        case "login":
+          wsLogin(wss, ws, msgObj);
+          break;
+        case "updateFriendList":
+          wsUpdateFriendList(wss, ws, msgObj);
+          break;
+        case "sendChatMessage":
+          wsSendChatMessage(wss, ws, msgObj);
+          break;
+      }
+    } catch (error) {
+      console.error("Error parsing JSON");
+    }
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+    wsOnClose(wss, ws);
+  });
+});
+
+server.listen(process.env.PORT, () => {
+  console.log("Server is listening on port: ", process.env.PORT);
+});

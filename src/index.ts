32063a8f4b79ab83b2025a3da80d813cf041ce16
benjamin5252 <david.l@amaryllo.us>
2024-02-17 13:51:50 +0800

    <fix><picture upload>

--- src/index.ts
+++ src/index.ts
@@ -36,7 +36,7 @@ app.use((req, res, next) => {
   next();
 });
 app.use(express.json());
-
+app.use(express.static("./public"));
 const allowedOrigins = [
   "http://localhost:3000",
   "https://benjamin5252.github.io",
@@ -53,8 +53,8 @@ const corsOptions = {
   },
 };
 app.use(cors(corsOptions));
+// app.use(cors());
 app.use(cookieParser());
-app.use(express.static("./public"));
 
 const storage = multer.diskStorage({
   destination: async function (req, file, cb) {
@@ -77,7 +77,9 @@ const uploadCallback = (req: MulterRequest, res) => {
   res.status(200).json(file.filename);
 };
 app.post("/api/upload", upload.single("file"), uploadCallback);
-
+app.get("/test", (req, res) => {
+  res.send("test");
+});
 app.use("/api/users", userRoutes);
 app.use("/api/posts", postRoutes);
 app.use("/api/comments", commentRoutes);

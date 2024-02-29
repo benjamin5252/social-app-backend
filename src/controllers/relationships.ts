3bd4faeb24d5ba8f760ee1bb76d691458cbb3545
benjamin5252 <david.l@amaryllo.us>
2024-02-29 13:44:01 +0800

    <feat><get friendlist all>

--- src/controllers/relationships.ts
+++ src/controllers/relationships.ts
@@ -1,78 +1,77 @@
-import { db } from "../config/mySQLConnect.js"
-import error from "../config/errors.js"
-import jwt from "jsonwebtoken"
-import { Express, Request, Response } from "express"
-import { MysqlError  } from 'mysql'
+import { db } from "../config/mySQLConnect.js";
+import error from "../config/errors.js";
+import jwt from "jsonwebtoken";
+import { Express, Request, Response } from "express";
+import { MysqlError } from "mysql";
 
-export const getRelationship = (req: Request, res: Response)=>{
+export const getRelationship = (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json({ result: false, ...error(10004) });
 
-  const token = req.cookies.accessToken
-  if(!token) return res.status(401).json({result: false, ...error(10004)})
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json({ result: false, ...error(10004) });
 
-  jwt.verify(token, "secretkey", (err, userInfo)=>{
-    if(err) return res.status(403).json({result: false, ...error(10004)})
+    const q = `SELECT followerUserId FROM relationships WHERE followedUserId=?`;
+    db.query(q, [req.query.followedUserId], (err: MysqlError, data: any[]) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json({
+        result: true,
+        content: data.map((relationship) => relationship.followerUserId),
+      });
+    });
+  });
+};
 
-    
-      const q = `SELECT followerUserId FROM relationships WHERE followedUserId=?`
-      db.query(q, [req.query.followedUserId], (err: MysqlError, data: any[])=>{
-        if (err) return res.status(500).json(err)
-        return res.status(200).json({result: true, content: data.map((relationship)=> relationship.followerUserId)})
-      })
-    
-  })
+export const addRelationship = (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json({ result: false, ...error(10004) });
 
-}
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json({ result: false, ...error(10004) });
 
-export const addRelationship = (req: Request, res: Response) =>{
-  const token = req.cookies.accessToken
-  if(!token) return res.status(401).json({result: false, ...error(10004)})
+    const q =
+      "INSERT INTO relationships (`followerUserId`, `followedUserId`) VALUES (?)";
 
-  jwt.verify(token, "secretkey", (err, userInfo)=>{
-    if(err) return res.status(403).json({result: false, ...error(10004)})
+    const values = [userInfo.id, req.query.followedUserId];
 
-    const q = 'INSERT INTO relationships (`followerUserId`, `followedUserId`) VALUES (?)'
+    db.query(q, [values], (err: MysqlError, data: any[]) => {
+      if (err) return res.status(500).json({ result: false, content: err });
+      return res.status(200).json({ result: true });
+    });
+  });
+};
 
-    const values = [
-      userInfo.id,
-      req.query.followedUserId
-    ]
+export const deleteRelationship = (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json({ result: false, ...error(10004) });
 
-    db.query(q, [values], (err: MysqlError, data: any[])=>{
-      if (err) return res.status(500).json({result: false, content: err})
-      return res.status(200).json({result: true})
-    })
-  })
-}
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json({ result: false, ...error(10004) });
 
-export const deleteRelationship = (req: Request, res: Response) =>{
-  const token = req.cookies.accessToken
-  if(!token) return res.status(401).json({result: false, ...error(10004)})
+    const q =
+      "DELETE FROM relationships WHERE followerUserId=? AND followedUserId=?";
 
-  jwt.verify(token, "secretkey", (err, userInfo)=>{
-    if(err) return res.status(403).json({result: false, ...error(10004)})
+    db.query(
+      q,
+      [userInfo.id, req.query.followedUserId],
+      (err: MysqlError, data: any[]) => {
+        if (err) return res.status(500).json({ result: false, content: err });
+        return res.status(200).json({ result: true });
+      }
+    );
+  });
+};
 
-    const q = 'DELETE FROM relationships WHERE followerUserId=? AND followedUserId=?'
+export const getFriendList = (req: Request, res: Response) => {
+  const token = req.cookies.accessToken;
+  if (!token) return res.status(401).json({ result: false, ...error(10004) });
 
-
-    db.query(q, [userInfo.id, req.query.followedUserId], (err: MysqlError, data: any[])=>{
-      if (err) return res.status(500).json({result: false, content: err})
-      return res.status(200).json({result: true})
-    })
-  })
-}
-
-export const getFriendList = (req: Request, res: Response)=>{
-
-  const token = req.cookies.accessToken
-  if(!token) return res.status(401).json({result: false, ...error(10004)})
-
-  jwt.verify(token, "secretkey", (err, userInfo)=>{
-    if(err) return res.status(403).json({result: false, ...error(10004)})
-    const q = `SELECT  * FROM relationships AS r JOIN users AS u ON (r.followedUserId = u.id) WHERE followerUserId=?`
-    db.query(q, [userInfo.id], (err: MysqlError, data: any[])=>{
-      if (err) return res.status(500).json(err)
-      return res.status(200).json({result: true, content: data})
-    })
-  })
-
-}
+  jwt.verify(token, "secretkey", (err, userInfo) => {
+    if (err) return res.status(403).json({ result: false, ...error(10004) });
+    const q = `SELECT  * FROM relationships AS r JOIN users AS u ON (r.followedUserId = u.id) WHERE followerUserId=? OR followedUserId=?`;
+    db.query(q, [userInfo.id, userInfo.id], (err: MysqlError, data: any[]) => {
+      if (err) return res.status(500).json(err);
+      return res.status(200).json({ result: true, content: data });
+    });
+  });
+};
